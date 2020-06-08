ac8b10b7f27127e016ac5a4d66b9ca9430f2e4f9
ranawaqas92 <m.r.waqas@hotmail.com>
2020-06-09 00:35:53 +0500

    add new file in child comp

--- src/Login.js
+++ src/Login.js
@@ -8,6 +8,8 @@ function Login(props){
             i m BootCamp2020 {props.Student}
             <br />
             i complt my 3rd assignment
+            <br />
+            thanks sir mohsin and all your teams
          </div>
     );
 }
